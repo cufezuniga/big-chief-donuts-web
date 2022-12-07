2ec8fa15d44546d8a3d3f9460cec8afdfe7ce7b5
Cufe Eremita <rodneyzhamilton@gmail.com>
2022-12-06 21:29:47 -0500

    Added more to CustomerSearch

--- src/components/CustomerSearch.js
+++ src/components/CustomerSearch.js
@@ -1,21 +1,22 @@
-import { TableCell, TableRow, TableBody } from '@mui/material';
+import { TableCell, TableRow, TableBody, TextField, Select } from '@mui/material';
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/Context';
 import { isLoggedIn } from '../utils/FormUtils';
 
 const CustomerSearch = () => {
    const { state, setState } = useContext(AppContext);
+   const [searchby, setSearchby] = useState();
    const [search, setSearch] = useState();
 
    let userInfo = [
-      { id: 'name', name: 'Name' },
-      { id: 'username', name: 'User name' },
-      { id: 'uid', name: 'User Id' }
+      { value: 'name', label: 'Name' },
+      { value: 'username', label: 'User name' },
+      { value: 'uid', label: 'User Id' }
    ];
 
    let items = userInfo.map((item, i) => {
       return (
-         <option key={i} value={item.id}>{item.name}</option>
+         <option key={i} value={item.id}>{item.label}</option>
       )
    });
 
@@ -23,19 +24,19 @@ const CustomerSearch = () => {
       <>
          {(isLoggedIn(state)
             ?
-            <div className='form border-black'>Search by user id, user name, first name, or last name:
+            <div className="form border-black">Search by user id, user name, first name, or last name:
                <TableBody>
                   <TableRow>
                      <TableCell >
-                        <label htmlFor='search'>Search by: </label>
+                        <label htmlFor="search">Search by: </label>
                      </TableCell>
                      <TableCell>
-                        <select id="search" onChange={setSearch}>
+                        <Select id="searchb" label="search by" onChange={e => setSearchby(e.target.value)}>
                            {items}
-                        </select>
+                        </Select>
                      </TableCell>
                      <TableCell>
-
+                        <TextField id="searchc" variant="standard" className='' />
                      </TableCell>
                   </TableRow>
                </TableBody>
